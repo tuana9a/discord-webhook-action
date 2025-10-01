b4b4baba9e9ec3aea059f55a83afc3d960bd98ee
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-10-01 08:53:33 +0000

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,26 @@
+const commonjs = require("@rollup/plugin-commonjs");
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const json = require("@rollup/plugin-json");
+const minify = require("@rollup/plugin-terser");
+
 const config = {
   input: "src/index.js",
-  output: {
-    esModule: true,
-    file: "dist/index.js",
-    format: "es",
-    sourcemap: true,
-  },
+  output: [
+    {
+      esModule: true,
+      file: "dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    {
+      esModule: true,
+      file: "dist/index.min.js",
+      format: "iife",
+      sourcemap: true,
+      plugins: [minify()],
+    },
+  ],
+  plugins: [json(), commonjs(), nodeResolve()],
 };
 
 module.exports = config;
